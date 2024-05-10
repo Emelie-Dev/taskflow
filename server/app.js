c00e66635fe0095926d07e200466bf5aedf51380
Emelie-Dev <godfatheremerie@gmail.com>
2024-05-11 00:15:40 +0100

    Updated the backend

--- server/app.js
+++ server/app.js
@@ -1,15 +1,77 @@
 // Core Modules
 
-// Custom Modules
-
-// Third Party Modules
+// Third party Modules
 
 import express from 'express';
 
 import { config } from 'dotenv';
 
-config({ path: './config.env' });
+import cors from 'cors';
+
+import helmet from 'helmet';
+
+import rateLimit from 'express-rate-limit';
+
+import cookieParser from 'cookie-parser';
+
+import sanitize from 'express-mongo-sanitize';
+
+import xss from 'xss-clean';
+
+import hpp from 'hpp';
+
+import compression from 'compression';
+
+import morgan from 'morgan';
+
+// Custom Modules
 
 const app = express();
+
+config({ path: './config.env' });
+
+// Middlewares
+
+// Implements Cors
+app.use(cors());
+app.options('*', cors());
+
+// Adds security headers
+app.use(helmet());
+
+// Implements rate limiting
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message:
+    'We have received too many request from this IP. Please try after one hour.',
+});
+app.use('/api', limiter);
+
+// Parses request body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+// Parses cookies
+app.use(cookieParser());
+
+// Sanitize mongo injections
+app.use(sanitize());
+
+// Sanitize html injections
+app.use(xss());
+
+// Prevents parameter pollution
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
+
+// Compresses response
+app.use(compression());
+
+// Displays response details in terminal
+app.use(morgan('dev'));
 
 export default app;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,50 @@
 // Core Modules
 
-// Custom Modules
+// Third party Modules
 
-// Third Party Modules
 import { config } from 'dotenv';
 
-config({ path: './config.env' });
+// Set environmental variables
+
+export const configEnv = () => config({ path: './config.env' });
+
+configEnv();
+
+// Handles uncaught exceptions(No business with the server)
+
+process.on('uncaughtException', (err) => {
+  console.log('\nError ', { name: err.name, message: err.message });
+  console.log('\nUncaught Exception Occured! Shutting down....\n');
+  process.exit(1);
+});
+
+// Custom Modules
 
 import app from './app.js';
 
-app.listen(process.env.PORT || 2005, () => {
-  console.log('Server has started....');
+// Starting the server
+
+const port = process.env.PORT || 3500;
+
+const server = app.listen(port, () => {
+  console.log(`App is running on port - ${port}`);
+});
+
+// Handles Promise Rejections
+
+process.on('unhandledRejection', (err) => {
+  console.log('\nError ', { name: err.name, message: err.message });
+  console.log('\nUnhandled Rejection Occured! Shutting down....\n');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+// Heroku specific
+
+process.on('SIGTERM', () => {
+  console.log('\nSIGTERM RECEIVED. Shutting down....');
+  server.close(() => {
+    console.log('\nProcess terminated!!\n');
+  });
 });
