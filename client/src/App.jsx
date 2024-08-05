b32af58e6d22c8b2bdcb12e6218fea0314d247de
Emelie-Dev <godfatheremerie@gmail.com>
2024-08-05 18:53:23 +0100

    Fix: Updated vite configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,8 +17,6 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 export const AuthContext = createContext();
 
-console.log(process.env.NODE_ENV);
-
 const App = () => {
   const [userData, setUserData] = useState({});
   const [isAuthenticated, setIsAuthenticated] = useState(null);
