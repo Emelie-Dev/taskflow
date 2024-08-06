100364667294214589d1c66af62315fe7c86095c
Emelie-Dev <godfatheremerie@gmail.com>
2024-08-06 13:23:06 +0100

    Fix: Updated API call settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Profile from './pages/Profile';
 import Notifications from './pages/Notifications';
 import ProjectItem from './pages/ProjectItem';
 import ProtectedRoute from './components/ProtectedRoute';
+import axios from 'axios';
 
 export const AuthContext = createContext();
 
